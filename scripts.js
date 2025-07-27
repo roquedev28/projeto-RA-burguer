9952c12f66ddbbf01c933f233d95ca43f3112a75
RoqueDev28 <roquedev23@gamil.com>
2025-07-27 15:00:47 -0300

    Aplicação do MAP

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 const list = document.querySelector('ul')
 const buttonShowAll = document.querySelector('.show-all')
-let myLi = ''
+const buttonMapAll = document.querySelector('.map-all')
 
-function showAll(){
-    menuOptions.forEach( product => {
+function showAll(productArray){
+    let myLi = ''
+    productArray.forEach( product => {
         myLi += `
             <li>
                 <img src=${product.src}>
@@ -14,9 +15,16 @@ function showAll(){
     })
     list.innerHTML = myLi
 }
+function mapAllItems(){
+    const newPrices = menuOptions.map ((product)=>({
+        ...product, //Spread Operator
+        price: product.price * 0.9,
+    }))
 
 
+    showAll(newPrices)
+}
 
 
-
-buttonShowAll.addEventListener('click', showAll)
+buttonShowAll.addEventListener('click', () => showAll (menuOptions))
+buttonMapAll.addEventListener('click', mapAllItems)
